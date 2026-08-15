caae5d3ea2b8d188112b85928ce3209fb5e14ab0
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2026-08-15 19:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (18.99%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x=str(x)
+        rev=x[::-1]
+        if (x==rev):
+            return True
+        else:
+            return False
+        
