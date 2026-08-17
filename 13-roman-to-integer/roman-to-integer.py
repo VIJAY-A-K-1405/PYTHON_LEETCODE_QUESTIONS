a1d797c56c1d88a3623ab93358a0bf8afab187b6
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2026-08-17 05:52:14 +0530

    Time: 7 ms (40.52%) | Memory: 19.3 MB (19.47%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -13,3 +13,16 @@ class Solution:
                 summ += v[s[i]]
                 i+=1
         return summ
+        
+        
+        
+        # summ = 0
+        # n = len(s)
+        # for i in range(n):
+        #     k = v[s[i]]
+        #     if i + 1 < n and k < v[s[i+1]]:
+        #         summ -= k
+        #     else:
+        #         summ += k
+        # return summ
+
